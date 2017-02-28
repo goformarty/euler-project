89dc7ba2851ce0bdbf6b9f32267596de85177630
GoForMarty <marty.stepien@gmail.com>
2017-02-28 15:19:23 +0100

    Shorter code

--- problem6.js
+++ problem6.js
@@ -1,16 +1,14 @@
 function sumSquareDifference(n) {
-  var sum = 0;
-  var sumOfSquares = 0;
+  var sum = sumOfSquares = 0;
   
   for (var i=1; i<=n; i++) {
-  sumOfSquares += i * i;
+  sumOfSquares += Math.pow(i,2);
 }
   for (var j=1; j<=n; j++) {
   sum += j;
 }
-  var squareOfSum = sum * sum;
+  var squareOfSum = Math.pow(sum,2);
   return squareOfSum - sumOfSquares;
-  
 }
 
 sumSquareDifference(100);
